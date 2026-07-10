05d2e7680bc2856681bdbd2ca9042696e22e71e6
OprahxD <shresthkumar56579@outlook.com>
2026-07-10 12:11:46 +0530

    Fixed redis

--- src/api/axios.js
+++ src/api/axios.js
@@ -17,4 +17,41 @@ const api = axios.create({
     timeout: 10000, // 10 second timeout so the frontend doesn't hang forever if backend stalls
 });
 
+// Helper function to recursively search for and upgrade http Cloudinary URLs to https
+const upgradeCloudinaryUrls = (data) => {
+    if (data === null || data === undefined) return data;
+    if (typeof data === 'string') {
+        if (data.startsWith('http://res.cloudinary.com')) {
+            return data.replace('http://', 'https://');
+        }
+        return data;
+    }
+    if (Array.isArray(data)) {
+        return data.map(upgradeCloudinaryUrls);
+    }
+    if (typeof data === 'object') {
+        const copy = {};
+        for (const key in data) {
+            if (Object.prototype.hasOwnProperty.call(data, key)) {
+                copy[key] = upgradeCloudinaryUrls(data[key]);
+            }
+        }
+        return copy;
+    }
+    return data;
+};
+
+// Response interceptor to fix mixed content warnings automatically for all retrieved media
+api.interceptors.response.use(
+    (response) => {
+        if (response.data) {
+            response.data = upgradeCloudinaryUrls(response.data);
+        }
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default api;
